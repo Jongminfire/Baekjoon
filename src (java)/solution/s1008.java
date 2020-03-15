f49277d4bec4e2fc67a380974bf74d38291d06f3
unknown <whd9612@sju.ac.kr>
2020-03-16 03:41:36 +0900

    20-03-16

---
+++ src (java)/solution/s1008.java
@@ -0,0 +1,19 @@
+package solution;
+
+import java.util.Scanner;
+
+public class s1008 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		double A = scanner.nextDouble();
+		double B = scanner.nextDouble();
+		
+		System.out.println(A/B);
+	}
+
+}
+
+//두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
---
+++ src (java)/solution/s11654.java
@@ -0,0 +1,19 @@
+package solution;
+
+import java.util.Scanner;
+
+public class s11654 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		String input = scanner.next();
+		int result = input.charAt(0);
+		
+		System.out.println(result);
+	}
+
+}
+
+//두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
