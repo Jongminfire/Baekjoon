54ec73d8c18c3becc99278e1817db0348c490ffc
unknown <whd9612@sju.ac.kr>
2020-03-16 03:14:58 +0900

    20-03-16

---
+++ src/solution/s10171.java
@@ -0,0 +1,15 @@
+package solution;
+
+public class s10171 {
+
+	public static void main(String[] args) {
+	
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
+
+// 아래 예제와 같이 고양이를 출력하시오. https://www.acmicpc.net/problem/10171
