cf672314694e5e72c3e5a4252a50263215e310cf
unknown <whd9612@sju.ac.kr>
2020-03-16 02:53:01 +0900

    200316

---
+++ src/solution/s1000.java
@@ -0,0 +1,19 @@
+package solution;
+
+import java.util.*;
+
+public class s1000 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner= new Scanner(System.in);
+		int A= scanner.nextInt();
+		int B= scanner.nextInt();
+		
+		System.out.println(A+B);
+		
+	}
+}
+
+
+//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오. https://www.acmicpc.net/problem/1000
---
+++ src/solution/s1001.java
@@ -0,0 +1,20 @@
+package solution;
+
+import java.util.Scanner;
+
+public class s1001 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int A,B;
+		A= scanner.nextInt();
+		B= scanner.nextInt();
+		
+		System.out.println(A-B);
+	}
+
+}
+
+//두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오. https://www.acmicpc.net/problem/1001
