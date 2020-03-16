7bd783641315cb8ba375df072dae8e607a87426e
unknown <whd9612@sju.ac.kr>
2020-03-17 00:22:13 +0900

    20-03-17

---
+++ C++/10872 (팩토리얼).cpp
@@ -0,0 +1,31 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int factorial(int num)
+//{
+//	if (num <= 1)
+//	{
+//		return 1;
+//	}
+//
+//	return num*factorial(num-1);
+//}
+//
+//int main()
+//{
+//	int N;
+//
+//	cin >> N;
+//
+//	cout << factorial(N);
+//
+//	return 0;
+//}
+//
+///*
+//
+//<문제>
+//0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+//
+//*/
