00de15c7ba615caff004767c1e166d45144b26f3
unknown <jongminfire@gmail.com>
2020-03-25 22:24:31 +0900

    20-03-25

--- C++/1330 (두 수 비교) .cpp
+++ C++/1330 (두 수 비교) .cpp
@@ -1,19 +1,24 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-	int A, B;
-	cin >> A >> B;
-
-	if (A < B)
-		cout << "<";
-	else if (A > B)
-		cout << ">";
-	else
-		cout << "==";
-
-	return 0;
-}
-
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int A, B;
+//	cin >> A >> B;
+//
+//	if (A < B)
+//		cout << "<";
+//	else if (A > B)
+//		cout << ">";
+//	else
+//		cout << "==";
+//
+//	return 0;
+//}
+//
+///*
+//
+//https://www.acmicpc.net/problem/1330
+//
+//*/
