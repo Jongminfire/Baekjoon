5646af9d587ba3137ca10cd60ae33efffb2734da
whd9612 <jongminfire@gmail.com>
2020-04-01 23:59:02 +0900

    20-04-01

--- C++/10818 (최소,최대).cpp
+++ C++/10818 (최소,최대).cpp
@@ -1,37 +1,37 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-int main()
-{
-	int s;
-	int max = -1000000;
-	int min = 1000000;
-
-	cin >> s;
-
-	vector<int> n(s);
-	
-	for (int i = 0;i <s;i++)
-	{
-		cin >> n[i];
-
-		if (n[i] < min)
-			min = n[i];
-		if (n[i] > max)
-			max = n[i];
-	}
-
-	cout << min << " " << max;
-
-	return 0;
-}
-
-/*
-
-N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
-
-https://www.acmicpc.net/problem/10818
-
-*/
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int s;
+//	int max = -1000000;
+//	int min = 1000000;
+//
+//	cin >> s;
+//
+//	vector<int> n(s);
+//	
+//	for (int i = 0;i <s;i++)
+//	{
+//		cin >> n[i];
+//
+//		if (n[i] < min)
+//			min = n[i];
+//		if (n[i] > max)
+//			max = n[i];
+//	}
+//
+//	cout << min << " " << max;
+//
+//	return 0;
+//}
+//
+///*
+//
+//N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+//
+//https://www.acmicpc.net/problem/10818
+//
+//*/
--- C++/2739 (구구단).cpp
+++ C++/2739 (구구단).cpp
@@ -1,18 +1,18 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-	int N;
-	cin >> N;
-
-	for (int i = 1;i <= 9;i++)
-	{
-		cout << N << " * " << i << " = " << N * i<<"\n";
-	}
-
-	return 0;
-}
-
-// N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오. 출력 형식에 맞춰서 출력하면 된다.
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int N;
+//	cin >> N;
+//
+//	for (int i = 1;i <= 9;i++)
+//	{
+//		cout << N << " * " << i << " = " << N * i<<"\n";
+//	}
+//
+//	return 0;
+//}
+//
+//// N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오. 출력 형식에 맞춰서 출력하면 된다.
