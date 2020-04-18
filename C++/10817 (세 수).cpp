09decdf41096c69f879f74661174b5e940932389
whd9612 <jongminfire@gmail.com>
2020-04-19 01:38:05 +0900

    20-04-19

---
+++ C++/10817 (세 수).cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int A, B, C;
+//	cin >> A >> B >> C;
+//	cout << (A < B ? (B < C ? B : (C<A?A:C)) : (A < C ? A : (C<B ? B : C)));
+//
+//	return 0;
+//}
+//
+///*
+//
+//첫째 줄에 세 정수 A, B, C가 공백으로 구분되어 주어진다. (1 ≤ A, B, C ≤ 100)
+//
+//https://www.acmicpc.net/problem/10817
+//
+//*/
