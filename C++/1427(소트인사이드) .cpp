9e7ddcd21f928ddeaf2ecd5921152245b6649713
whd9612 <jongminfire@gmail.com>
2020-04-18 23:16:21 +0900

    20-04-18

---
+++ C++/1427(소트인사이드) .cpp
@@ -0,0 +1,36 @@
+//#include <iostream>
+//#include <algorithm>
+//#include <string>
+//
+//using namespace std;
+//
+//bool cmp(char a, char b)
+//{
+//	if (a < b)
+//		return false;
+//	else
+//		return true;
+//}
+//
+//int main()
+//{
+//	string s;
+//
+//	cin >> s;
+//
+//	sort(s.begin(), s.end(),cmp);
+//
+//	for (int i = 0;i < s.size();i++)
+//	{
+//		cout << s[i];
+//	}
+//
+//	return 0;
+//}
+//
+///*
+//
+//배열을 정렬하는 것은 쉽다. 수가 주어지면, 그 수의 각 자리수를 내림차순으로 정렬해보자.
+//첫째 줄에 정렬하고자하는 수 N이 주어진다. N은 1,000,000,000보다 작거나 같은 자연수이다.
+//
+//*/
