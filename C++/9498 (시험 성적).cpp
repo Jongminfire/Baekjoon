8a050ff0aa312f269497ff721e7b8a77c7435ae3
whd9612 <jongminfire@gmail.com>
2020-04-22 00:42:36 +0900

    20-04-22

---
+++ C++/9498 (시험 성적).cpp
@@ -0,0 +1,31 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int N;
+//	cin >> N;
+//
+//	if (N >= 90 && N <= 100)
+//		cout << "A";
+//	else if (N >= 80 && N <= 89)
+//		cout << "B";
+//	else if (N >= 70 && N <= 79)
+//		cout << "C";
+//	else if (N >= 60 && N <= 69)
+//		cout << "D";
+//	else
+//		cout << "F";
+//
+//	return 0;
+//}
+//
+///*
+//
+//시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+//첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+//
+//https://www.acmicpc.net/problem/9498
+//
+//*/
