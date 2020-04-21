d4752daff9171d8a236bb29dade4afdc66ca753c
whd9612 <jongminfire@gmail.com>
2020-04-21 23:54:17 +0900

    20-04-21

---
+++ C++/8393(합).cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int N;
+//	int result = 0;
+//	cin >> N;
+//
+//	for (int i = 1;i <= N;i++)
+//	{
+//		result += i;
+//	}
+//
+//	cout << result;
+//
+//	return 0;
+//}
+///*
+//
+//n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+//
+//*/
