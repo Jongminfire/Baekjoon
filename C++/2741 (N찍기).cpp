36ef1eaffbd9245f06b71ba46f2cf7b7cb4807e2
whd9612 <jongminfire@gmail.com>
2020-04-29 01:00:06 +0900

    20-04-29

---
+++ C++/2741 (N찍기).cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int N;
+//	cin >> N;
+//
+//	for (int i = 1; i < N + 1; i++)
+//	{
+//		cout << i << "\n";
+//	}
+//
+//	return 0;
+//}
+//
+///*
+//
+//자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+//
+//*/
