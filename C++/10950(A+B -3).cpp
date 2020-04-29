43ee6b2508f08cec4ffc2c5c44ac8f3a4abdde7c
whd9612 <jongminfire@gmail.com>
2020-04-30 00:38:30 +0900

    20-04-30

---
+++ C++/10950(A+B -3).cpp
@@ -0,0 +1,31 @@
+//#include <iostream>
+//#include <vector>
+//#include <utility>	// pair 쓰기 위함
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int N;
+//	cin >> N;
+//	vector<pair<int, int>> v(N);
+//
+//	for (int i = 0;i < N;i++)
+//	{
+//		cin >> v[i].first >> v[i].second;
+//	}
+//
+//	for (int i = 0; i < N;i++)
+//	{
+//		cout << v[i].first + v[i].second << "\n";
+//	}
+//
+//	return 0;
+//}
+//
+///*
+//
+//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+//https://www.acmicpc.net/problem/10950
+//
+//*/
