ce8bc159f4bdd77085dd7fb0a6f183f2c030b00b
whd9612 <jongminfire@gmail.com>
2020-05-02 00:53:29 +0900

    20-05-02

---
+++ C++/4153(직각삼각형).cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int A, B, C;
+//	int max = 0;
+//
+//	while (1)
+//	{
+//		cin >> A >> B >> C;
+//
+//		if (A == 0 && B == 0 && C == 0)
+//			break;
+//		
+//		if (A*A == B * B + C * C || B * B == A * A + C * C || C * C == A * A + B * B)
+//			cout << "right\n";
+//		else
+//			cout << "wrong\n";
+//	}
+//
+//	return 0;
+//}
