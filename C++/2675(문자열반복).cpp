ecd72ed4021980f1b32ed348461131e518471714
whd9612 <jongminfire@gmail.com>
2020-05-08 04:00:13 +0900

    20-05-08

---
+++ C++/2675(문자열반복).cpp
@@ -0,0 +1,38 @@
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int N;
+//	cin >> N;
+//
+//	string s;
+//	int num;
+//
+//	for (int i = 0;i < N;i++)
+//	{
+//		cin >> num;
+//		getchar();
+//		getline(cin, s);
+//
+//		for (int i = 0;i<s.size();i++)
+//		{
+//			for (int j = 0;j < num;j++)
+//			{
+//				cout << s[i];
+//			}
+//		}
+//
+//		cout << "\n";
+//	}
+//
+//	return 0;
+//}
+//
+///*
+//
+//첫째 줄에 테스트 케이스의 개수 T(1 ≤ T ≤ 1,000)가 주어진다. 각 테스트 케이스는 반복 횟수 R(1 ≤ R ≤ 8), 문자열 S가 공백으로 구분되어 주어진다. S의 길이는 적어도 1이며, 20글자를 넘지 않는다.
+//
+//*/
