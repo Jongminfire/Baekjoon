3baa64dafc30a4a02190b5142ff08b126e3e9ca3
whd9612 <jongminfire@gmail.com>
2020-05-12 00:01:43 +0900

    20-05-12

---
+++ C++/1085(직사각형에서 탈출).cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+//#include <vector>
+//#include <math.h>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int x, y, w, h;
+//
+//	cin >> x >> y >> w >> h;
+//
+//	cout << min(min(abs(h - y), y), min(abs(w - x), x));
+//
+//	return 0;
+//}
+//
+///*
+//
+//첫째 줄에 x y w h가 주어진다. w와 h는 1,000보다 작거나 같은 자연수이고, x는 1보다 크거나 같고, w-1보다 작거나 같은 자연수이고, y는 1보다 크거나 같고, h-1보다 작거나 같은 자연수이다.
+//https://www.acmicpc.net/problem/1085
+//
+//*/
