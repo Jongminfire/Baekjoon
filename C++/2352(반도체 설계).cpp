e8e18c6d1ec61b70e9404fb53d428a5dc7da0be7
whd9612 <jongminfire@gmail.com>
2020-06-14 01:41:48 +0900

    20-06-14

---
+++ C++/2352(반도체 설계).cpp
@@ -0,0 +1,47 @@
+//#include <iostream>
+//#include <algorithm>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cout.tie(NULL);
+//
+//	int n;
+//	cin >> n;
+//
+//	vector<int> v(n);
+//	vector<int> LIS;
+//
+//	for (int i = 0;i < n;i++)
+//	{
+//		cin >> v[i];
+//	}
+//
+//	for (int i = 0;i < n;i++)
+//	{
+//		int find = v[i];
+//		auto it = lower_bound(LIS.begin(), LIS.end(), find);
+//
+//		if (it == LIS.end())
+//			LIS.push_back(find);
+//		else
+//			*it = find;
+//	}
+//
+//	cout << LIS.size() << "\n";
+//
+//	return 0;
+//}
+//
+///*
+//
+//LIS 문제
+//기본 LIS문제랑 코드 같음
+//
+//https://www.acmicpc.net/problem/2352
+//
+//*/
