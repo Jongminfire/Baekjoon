7d46694e7cfa173eaf885da7a4a377cfa4790ace
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2020-07-09 00:35:20 +0900

    20-07-09

---
+++ C++/12015(가장 긴 증가하는 부분 수열2).cpp
@@ -0,0 +1,43 @@
+//#include <iostream>
+//#include <algorithm>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//
+//	vector<int> v(n);
+//	vector<int> LIS;
+//
+//	for (int i = 0;i < n;i++)
+//	{
+//		cin >> v[i];
+//	}
+//
+//	for (int i = 0;i < n;i++)
+//	{
+//		int find = v[i];
+//		auto it = lower_bound(LIS.begin(), LIS.end(), find);
+//
+//		if (it == LIS.end())
+//			LIS.push_back(find);
+//		else
+//			*it = find;
+//	}
+//
+//	cout << LIS.size() << "\n";
+//
+//	return 0;
+//}
+//
+///*
+//
+//LIS 문제
+//11053이랑 코드 같음
+//
+//https://www.acmicpc.net/problem/12015
+//
+//*/
