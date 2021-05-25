5d3a787562092d1260f40a2fe86add6883a16304
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2021-05-25 17:07:45 +0900

    m개의 치킨집을 없앨 때 최소 치킨거리 구하기

---
+++ Python/15686 (치킨배달, 브루트포스).py
@@ -0,0 +1,25 @@
+from itertools import combinations
+
+n, m = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
+house = []
+chicken = []
+ans = 2500
+
+for i in range(n):
+    for j in range(n):
+        if board[i][j] == 1:
+            house.append((i, j))
+        if board[i][j] == 2:
+            chicken.append((i, j))
+
+for c in combinations(chicken, m):
+    temp = [2500 for _ in range(len(house))]
+
+    for i, (hx, hy) in enumerate(house):
+        for cx, cy in c:
+            temp[i] = min(abs(hx-cx)+abs(hy-cy), temp[i])
+
+    ans = min(ans, sum(temp))
+
+print(ans)
