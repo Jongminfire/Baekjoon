a953447ba923df1c2ae8ed4c9520f6268711096d
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2021-05-25 20:31:08 +0900

    n번째 피보나치 수까지 피보나치 수의 제곱의 합 구하기

---
+++ Python/11440 (피보나치 수의 제곱의 합,분할정복).py
@@ -0,0 +1,37 @@
+n = int(input())
+init = [[1, 1], [1, 0]]
+ans = 0
+
+
+# 행렬의 곱셈
+def multi(m1, m2):
+    ans = []
+    for i in range(len(m1)):
+        lst = []
+        for j in range(len(m2[0])):
+            temp = 0
+            for k in range(len(m1[0])):
+                temp += m1[i][k] * m2[k][j]
+            lst.append(temp % 1000000007)
+        ans.append(lst)
+
+    return ans
+
+
+def squared(matrix, p):
+    if p == 1:
+        return matrix
+
+    div = squared(matrix, p//2)
+    squared_div = multi(div, div)
+
+    if p % 2 == 0:
+        return squared_div
+    else:
+        return multi(squared_div, matrix)
+
+
+# n이 주어졌을 때 피보나치 수의 제곱의 합은 F(n)*F(n-1)
+ans = squared(init, n)[0][1] * squared(init, n+1)[0][1] % 1000000007
+
+print(ans)
